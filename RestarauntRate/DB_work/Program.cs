77e9e66196d2f7bd380bfd620bfb3d34fc10194e
Ruslan Avsenin <ruslan.avsenin@gmail.com>
2015-12-12 16:14:07 +0200

    fileinput

--- RestarauntRate/DB_work/Program.cs
+++ RestarauntRate/DB_work/Program.cs
@@ -5,68 +5,105 @@ using System.Text;
 using System.Threading.Tasks;
 using Domain.Entities;
 using Domain.Concrete;
-
+using System.Drawing;
 
 namespace DB_work
 {
     class Program
     {
+        public static void SaveImageAsJPG(System.Drawing.Image image, string path)
+        {
+            image = System.Drawing.Image.FromFile(path);
+
+            image.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
         static void Main(string[] args)
         {
             using (var db = new EFDbContext())
             {
-                var user = new User() { UserName = "proverka", Password = "proverka", Email = "test@gmail.com", Role = User.UserRole.Inactive, RegisterDate = DateTime.Now };
-                var user1 = new User() { UserName = "admin", Password = "secret", Email = "ruslan.avsenin@gmail.com", Role = User.UserRole.Admin, RegisterDate = DateTime.Now };
-                var user2 = new User() { UserName = "Elena_Tinkovan", Password = "1111", Email = "tioraelena@gmail.com", Role = User.UserRole.Moderator, RegisterDate = DateTime.Now };
+                //   var user = new User() { UserName = "proverka", Password = "proverka", Email = "test@gmail.com", Role = User.UserRole.Inactive, RegisterDate = DateTime.Now };
+                // var user1 = new User() { UserName = "admin", Password = "secret", Email = "ruslan.avsenin@gmail.com", Role = User.UserRole.Admin, RegisterDate = DateTime.Now };
+                //var user2 = new User() { UserName = "Elena_Tinkovan", Password = "1111", Email = "tioraelena@gmail.com", Role = User.UserRole.Moderator, RegisterDate = DateTime.Now };
+               
 
-                db.Users.Add(user);
-                db.Users.Add(user1);
-                db.Users.Add(user2);
-                //var rest = new Restaraunt() { MaintenanceRate = 1, InteriorRate = 3, KitchenRate = 4, AddedDate = DateTime.Now, RestarauntType = Restaraunt.RestType.Cafe, Latitude = "1", Longitude = "2" };
-                //db.Restaraunts.Add(rest);
+                //db.Users.Add(user);
+                //db.Users.Add(user1);
+                //db.Users.Add(user2);
+               // var rest = new Restaraunt() { MaintenanceRate = 1, InteriorRate = 3, KitchenRate = 4, AddedDate = DateTime.Now, RestarauntType = Restaraunt.RestType.Cafe, Latitude = "1", Longitude = "2" };
+               // db.Restaraunts.Add(rest);
                 //var image = new Image() { ImageMimeType = "123", ImageData = null };
-                //var restLangs = new RestarauntLang() { Country = "test", Address = "test", Locality = "test", Name = "test", Region = "test", Review = "test" };
-                //var language = new Language() { Name = "russian", Code = "rus" };
+               // var restLangs = new RestarauntLang() { Country = "rusTest", Address = "rusTest", Locality = "rusTest", Name = "rusTest", Region = "rusTest", Review = "rusTest" };
+                //restLangs.LanguageID = 6;
+                //rest.RestLangs.Add(restLangs);
+                //db.Restaraunts.Add(rest);
+            //    var restLangs1 = new RestarauntLang() { Country = "engTest", Address = "engTest", Locality = "engTest", Name = "engTest", Region = "engTest", Review = "engTest" };
+             //   var language = new Language() { Name = "russian", Code = "rus" };
+           //     var language1 = new Language() { Name = "english", Code = "eng" };
                 //var comment = new Comment() { Name = "bla bla", RestarauntID = 1, AddedDate = DateTime.Now, Review = "cool" };
                 //var comment1 = new Comment() { Name = "gav gav", RestarauntID = 1, AddedDate = DateTime.Now, Review = "nice" };
-                //rest.Images.Add(image);
-                //rest.RestLangs.Add(restLangs);
-                //rest.Comment.Add(comment);
-                //rest.Comment.Add(comment1);
-                //language.RestLangs.Add(restLangs);
-                //db.Restaraunts.Add(rest);
-                //db.Languages.Add(language);                           
+              /*  rest.RestLangs.Add(restLangs);
+                rest.RestLangs.Add(restLangs1);
+                language.RestLangs.Add(restLangs);
+                language1.RestLangs.Add(restLangs1);
+                db.Restaraunts.Add(rest);
+                db.Languages.Add(language);
+                db.Languages.Add(language1);  */
+                
+                                      
                 try
                 {
                     //     EFUserRepository test = new EFUserRepository();
-
-                    // User u = db.Users.Where(usr => usr.UserID == 2).First();
-                    //   u.Role = User.UserRole.Moderator;
+                     var lol = db.Restaraunts.Where(rest => rest.RestarauntID == 18).First();
+                    var lol2 = db.Restaraunts.Where(rest => rest.RestarauntID == 19).First();
+                    var lol1 = db.Restaraunts.Where(rest => rest.RestarauntID == 20).First();
+                    db.Restaraunts.Remove(lol);
+                    db.Restaraunts.Remove(lol1);
+                    db.Restaraunts.Remove(lol2);
+                    //  var lang = db.Languages.Where(l => l.LanguageID == 1).First();
+                    // db.Languages.Remove(lang);
+                    // User u = db.Users.Where(usr => usr.UserID == 7).First();
+                    // u.Role = User.UserRole.Moderator;
                     //test.SaveUser(u);
-                    // db.SaveChanges();
+                    db.SaveChanges();
                     //User usr = db.Users.Where(user => user.Email == "tioraelena@gmail.com").First();
                     //usr.Role = User.UserRole.Moderator;
-                 //   Image tmp = db.Images.Where(img => img.ImageID == 12).First();
-                   // Image tmp1 = db.Images.Where(img => img.ImageID == 5).First();
-                  //  db.Images.Remove(tmp);
-                   // db.Images.Remove(tmp1);
-                    db.SaveChanges();
-                        //   User user = context.Users.Where(usr => usr.Email == "ruslan.avsenin@gmail.com").First();
-                        // Console.WriteLine(user.UserName + " " + user.Password + " " + user.Role);
+                   // foreach(Image img in db.Images)
+                  /*  {
+                        db.Images.Remove(img);
+                    }*/
+                    // Console.WriteLine(DateTime.Now.ToFileTime());
+                   /* var files = System.IO.Directory.GetFiles(@"E:\Ruslan\Web\Projects\RestRate\RestarauntRate\DB_work\start");
+                    int count = 0;
+                    foreach (var file in files)
+                    {
+                        Bitmap tempBmp = new Bitmap(file);
+                        Bitmap bmp = new Bitmap(tempBmp, 807, 605);
 
-                       // User user = context.Users.Find(1);
-                       // user.Role = User.UserRole.Moderator;
-                        //context.SaveChanges();
-                        //   User dbEntry = db.Users.Where(usr => usr.UserName == "admin").First();
-                        // Console.WriteLine(dbEntry.UserName + dbEntry.Password + dbEntry.Role);
-                        // foreach(User usr in db.Users)
-                        //{
-                        //    Console.WriteLine(usr.UserName + usr.Password + usr.Role);
-                        //}
-                        //foreach (var u in db.Users)
-                        // {
-                        // Console.WriteLine("Name: {0} Password: {1} Email: {2} Role: {3}", u.UserName, u.Password, u.Email, u.Role);
-                        //}                   
+                        bmp.Save(
+                        @"E:\Ruslan\Web\Projects\RestRate\RestarauntRate\DB_work\res\" + count + ".jpg",
+                        System.Drawing.Imaging.ImageFormat.Jpeg);
+                        count++;
+
+                    }                  */
+                    //Image tmp = db.Images.Where(img => img.ImageID == 5).First();
+                    //Image tmp1 = db.Images.Where(img => img.ImageID == 6).First();
+                    // db.SaveChanges();
+                    //   User user = context.Users.Where(usr => usr.Email == "ruslan.avsenin@gmail.com").First();
+                    // Console.WriteLine(user.UserName + " " + user.Password + " " + user.Role);
+
+                    // User user = context.Users.Find(1);
+                    // user.Role = User.UserRole.Moderator;
+                    //context.SaveChanges();
+                    //   User dbEntry = db.Users.Where(usr => usr.UserName == "admin").First();
+                    // Console.WriteLine(dbEntry.UserName + dbEntry.Password + dbEntry.Role);
+                    // foreach(User usr in db.Users)
+                    //{
+                    //    Console.WriteLine(usr.UserName + usr.Password + usr.Role);
+                    //}
+                    //foreach (var u in db.Users)
+                    // {
+                    // Console.WriteLine("Name: {0} Password: {1} Email: {2} Role: {3}", u.UserName, u.Password, u.Email, u.Role);
+                    //}                   
                 }
                 catch
                 {
--- RestarauntRate/RestRate/Controllers/AdminController.cs
+++ RestarauntRate/RestRate/Controllers/AdminController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 using System.Web.Security;
 using Domain.Concrete;
 using RestRate.ModelView;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 
 namespace RestRate.Controllers
 {
@@ -19,14 +21,16 @@ namespace RestRate.Controllers
         private IImageRepository imageRepository;
         private IRestarauntLangRepository restLangRepository;
         private IUserRepository userRepository;
+        private ILanguageRepository langRepository;
         public AdminController(IRestarauntRepository restRepository, IRestarauntLangRepository restLangRepository,
-                                ICommentRepository commentRepository, IImageRepository imageRepository, IUserRepository userRepository)
+                                ICommentRepository commentRepository, IImageRepository imageRepository, IUserRepository userRepository, ILanguageRepository langRepository)
         {
             this.restRepository = restRepository;
             this.restLangRepository = restLangRepository;
             this.commentRepository = commentRepository;
             this.imageRepository = imageRepository;
-            this.userRepository = userRepository;    
+            this.userRepository = userRepository;
+            this.langRepository = langRepository;
         }
         // GET: Admin
         public ViewResult Index()
@@ -40,82 +44,139 @@ namespace RestRate.Controllers
             FormsAuthentication.SignOut();
             return RedirectToAction("Login", "Account");
         }
+        List<Image> Images = new List<Image>();
         [HttpPost]
         public ActionResult AddRestaurant(RestaurantData data)
         {
-            
+            var tmp = Images;
             if (!data.Equals(null))
             {
                 try
                 {
+                    /* using (var context = new EFDbContext())
+                     {
 
-                    Restaraunt NewRestaraunt = data.RestarauntData;
-                    NewRestaraunt.RestLangs.Add(data.RestaurantLangData);
+                         Restaraunt NewRestaraunt = data.RestarauntData;
+                         RestarauntLang NewRestLang = data.RestaurantLangData;
+
+                         Language Test = context.Languages.Where(t => t.LanguageID == 7).First();
+
+                         NewRestaraunt.AddedDate = DateTime.Now;
+                         NewRestaraunt.Latitude = "2";
+                         NewRestaraunt.Longitude = "3";
+                         NewRestaraunt.RestarauntType = Restaraunt.RestType.Bar;
+
+                         restRepository.SaveRestaraunt(NewRestaraunt);
+
+                         NewRestLang.LanguageID = 7;
+                         NewRestLang.RestarauntID = NewRestaraunt.RestarauntID;
+
+                         restLangRepository.SaveRestarauntLang(NewRestLang);
 
 
-
-
+                         return Json(new { result = "success", id = NewRestaraunt.RestarauntID });
+                     }*/
+                    return null;
                     /* HttpPostedFileBase MyFile = null;
-            int Number = Request.Files.Count;
-            Restaraunt tmp;
-            string ForSaving = Server.MapPath("~/Content/Images/RestaurantImages");
-            using (var context = new EFDbContext())
-            {
-
-                if (Number != 0)
+                int Number = Request.Files.Count;
+                Restaraunt tmp;
+                string ForSaving = Server.MapPath("~/Content/Images/RestaurantImages");
+                using (var context = new EFDbContext())
                 {
-                    for (int i = 0; i < Number; i++)
-                    {
 
-                        MyFile = Request.Files[i];
-                        string FileName = "Restaraunt" + 0 + "Image" + i + System.IO.Path.GetExtension(MyFile.FileName);
-                        string url = System.IO.Path.Combine(ForSaving, FileName);
-                        Image New = new Image() { Url = url };
-                        New.RestarauntID = 0;                          
-                        NewRestaraunt.Images.Add(New);   
-                        restRepository.SaveRestaraunt(tmp);
-                        MyFile.SaveAs(url);
+                    if (Number != 0)
+                    {
+                        for (int i = 0; i < Number; i++)
+                        {
+
+                            MyFile = Request.Files[i];
+                            string FileName = "Restaraunt" + 0 + "Image" + i + System.IO.Path.GetExtension(MyFile.FileName);
+                            string url = System.IO.Path.Combine(ForSaving, FileName);
+                            Image New = new Image() { Url = url };
+                            New.RestarauntID = 0;                          
+                            NewRestaraunt.Images.Add(New);   
+                            restRepository.SaveRestaraunt(tmp);
+                            MyFile.SaveAs(url);
+                        }
                     }
                 }
-            }
-            */
-                    restRepository.SaveRestaraunt(NewRestaraunt);
-                    return Json(new { result = "success", message = "Restaurant was successfully added." });
+                */
                 }
-                catch
+                catch (DbEntityValidationException dbEx)
                 {
-                    return Json(new { result = "error", message = "Some troubles was happend with database." });
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            Trace.TraceInformation("Property: {0} Error: {1}",
+                                                    validationError.PropertyName,
+                                                    validationError.ErrorMessage);
+                        }
+                    }
+                    return Json(new { result = "error", message = "Ooooooops! Some troubles was happened with DB." });
                 }
             }
-            return Json(new { result = "JSON IS NULL" });
+            return Json(new { result = "error", message = "JSON IS NULL" });
         }
+
         [HttpPost]
-        public ActionResult UploadFile()
+        public ActionResult Index(IEnumerable<HttpPostedFileBase> files)//ImageData data)
         {
-            HttpPostedFileBase MyFile = null;
-            int Number = Request.Files.Count;
-            Restaraunt tmp;
-            string ForSaving = Server.MapPath("~/Content/Images/RestaurantImages");
-            using (var context = new EFDbContext())
+           // int RestarauntID = data.RestarauntID;
+            var res = new List<HttpPostedFileBase>();
+            foreach (var file in files)//data.files)
             {
-                tmp = context.Restaraunts.Where(rest => rest.RestarauntID == 1).First();
-
-                if (Number != 0)
+                if (file.ContentLength < 0)
                 {
-                    for (int i = 0; i < Number; i++)
-                    {
-
-                        MyFile = Request.Files[i];
-                        string FileName = "Restaraunt" + 0 + "Image" + i + System.IO.Path.GetExtension(MyFile.FileName);
-                        string url = System.IO.Path.Combine(ForSaving, FileName);
-                        Image New = new Image() { Url = url };
-                        New.RestarauntID = 0;                        
-                        tmp.Images.Add(New);      
-                        restRepository.SaveRestaraunt(tmp);
-                        MyFile.SaveAs(url);
-                    }
+                    continue;
                 }
+                res.Add(file);
+                // do something with the file
             }
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(IEnumerable<HttpFileCollectionBase> files)
+        {
+            var a = Request.Files;
+            // TimeStamp
+            long ticks = DateTime.UtcNow.Ticks - DateTime.Parse("01/01/1970 00:00:00").Ticks;
+            ticks /= 10000000; //Convert windows ticks to seconds
+            var timestamp = ticks.ToString();
+            //
+            HttpPostedFileBase MyFile = Request.Files[0];
+
+            string FileName = 0 + timestamp + System.IO.Path.GetExtension(MyFile.FileName);
+
+
+            string ForSaving = Server.MapPath("~/Content/Images/RestaurantImages/"+0);
+            string Url = System.IO.Path.Combine(ForSaving, FileName);
+            Image New = new Image() { Url = Url };
+            Images.Add(New);
+
+            /* Restaraunt tmp;
+             string ForSaving = Server.MapPath("~/Content/Images/RestaurantImages");
+             using (var context = new EFDbContext())
+             {
+                 tmp = context.Restaraunts.Where(rest => rest.RestarauntID == 1).First();
+
+                 if (Number != 0)
+                 {
+                     for (int i = 0; i < Number; i++)
+                     {
+
+                         MyFile = Request.Files[i];
+                         string FileName = "Restaraunt" + 0 + "Image" + i + System.IO.Path.GetExtension(MyFile.FileName);
+                         string url = System.IO.Path.Combine(ForSaving, FileName);
+                         Image New = new Image() { Url = url };
+                         New.RestarauntID = 0;                        
+                         tmp.Images.Add(New);      
+                         restRepository.SaveRestaraunt(tmp);
+                         MyFile.SaveAs(url);
+                     }
+                 }
+             }*/
             return null;
         }
         [HttpPost]
