4bce194bcf0cdbd51acf3f277b21a07ea265eeef
Crispried <ruslan.avsenin@gmail.com>
2015-12-21 17:11:04 +0200

    some changes in controllers

--- RestarauntRate/RestRate/Controllers/HomeController.cs
+++ RestarauntRate/RestRate/Controllers/HomeController.cs
@@ -63,13 +63,15 @@ namespace RestRate.Controllers
                         Name = RestarauntLangList[i].Name,
                         Country = RestarauntLangList[i].Country,
                         Locality = RestarauntLangList[i].Locality,
-                        Region = RestarauntLangList[i].Region
+                        Region = RestarauntLangList[i].Region,                        
                     };
                     FullObject = new RestIDNameFullAddressRates()
                     {
                         InteriorRate = RestarauntList[i].InteriorRate,
                         KitchenRate = RestarauntList[i].KitchenRate,
                         MaintenanceRate = RestarauntList[i].MaintenanceRate,
+                        Longitude = RestarauntList[i].Longitude,
+                        Latitude = RestarauntList[i].Latitude,
                         RestaurantIDNameFullAddress = PartOfObject                       
                     };
                     RestIDNameFullAddressRates.Add(FullObject);
